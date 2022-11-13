76f7b9567077dad5d7eb846e7d04bcaa88d1725a
youthmn <banghmin@hanmail.net>
2022-11-13 20:34:53 +0900

    ✨ feat:button컴포넌트

---
+++ client/src/components/Card/MintCard.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const MintCardStyle = styled.button`
+  height: ${(props) => (props.height ? props.height : "auto")};
+  width: ${(props) => (props.width ? props.width : "auto")};
+  color: hsl(0, 0%, 32%);
+  background-color: hsl(0, 0%, 100%);
+  border: 1px solid hsl(180, 32%, 54%);
+  border-radius: 35px;
+  box-shadow: 0px 0px 4px 2px #63aeae;
+  font-size: 14px;
+`;
+
+const MintCard = ({ text, height, width, handleSubmit }) => {
+  return (
+    <MintCardStyle width={width} height={height} onClick={handleSubmit}>
+      {text}
+    </MintCardStyle>
+  );
+};
+
+export default MintCard;
+
+{
+  /* <BlueButton text="Ask Question" width="300px "height="200px" /> */
+}
