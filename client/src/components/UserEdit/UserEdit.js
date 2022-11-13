c478310373934a8e40a05e5deb735247e7e0c36c
youthmn <banghmin@hanmail.net>
2022-11-13 20:35:20 +0900

    ✨ feat:UserEdit 만드는중

---
+++ client/src/components/UserEdit/UserEdit.js
@@ -0,0 +1,15 @@
+// import styled from "styled-components";
+
+export const UserEdit = () => {
+  return (
+    <div>
+      <div>IMG</div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <button>회원정보 수정버튼</button>
+    </div>
+  );
+};
